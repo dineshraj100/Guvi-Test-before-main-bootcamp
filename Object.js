b2712c6ee73dbe0d6c7d4e6675c5c4d3eb5058da
dineshraj100 <58807848+dineshraj100@users.noreply.github.com>
2021-11-17 10:41:50 +0530

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,28 @@
+var Ram_details =[{
+    "memory_summary":[{
+        "max_capacity": "2048 MBytes",
+        "max_memory_module_size": "4096 MBytes",
+        "memory_slots": 4,
+        "error_correction": "None",
+        "DRAM_frequency": "199.0 MHz",
+        "memory_timings": "3-3-3-8(CL-RCD-RP-RAS)"
+    }]
+}, {
+    "device_locator": [{
+        "device_locator": "slot 1",
+        "manufacturer": "Hyundai Electronics",
+        "part_number": "HYMD264 646D8J-D43",
+        "serial_number": "FFFF7240",
+        "capacity": "512 MBytes",
+        "memory_type": "DDR (PC3200)",
+        "speed": "200 MHz",
+        "supported_frequencies": "133 MHz, 166MHz",
+        "memory_timings_1": "2-2-2-6-0 at 133 MHz",
+        "memory_timings_2": "2-3-3-7-0 at 166 MHz",
+        "memory_timings_3": "3-3-3-8-0 at 200 MHz",
+        "data_width": "64 bits",
+        "manufacturing_date": "2005,week 39",
+        "EPP_SPD_support": "No",
+        "XMP_SPD_support": "No"
+    }]
+}];
